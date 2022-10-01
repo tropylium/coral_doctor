c32cc92d04ed05c50090968fb87bce79b1194ccc
tropylium <alexdang@mit.edu>
2022-10-01 15:36:40 -0400

    test build

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
@@ -11,7 +11,9 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
       <BrowserRouter>
-          <App />
+          <Routes>
+              <Route path="/" element={<App />} />
+          </Routes>
       </BrowserRouter>,
   </React.StrictMode>
 );
